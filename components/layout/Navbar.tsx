59f460411475efc7b242cb2c811c086c65914afb
Theguardians58 <125973002+Theguardians58@users.noreply.github.com>
2025-09-17 19:03:57 +0530

    Update Navbar.tsx

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -39,8 +39,8 @@ const Navbar = () => {
   <Image
     src="/logo.png" // <-- CHANGE THIS if your logo has a different name
     alt="Éclat Lingerie Logo"
-    width={100}
-    height={40}
+    width={140}
+    height={60}
     priority
   />
 </Link>
