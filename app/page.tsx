20c6d0b2f871071ceb431569fc11042fdcf7a7bc
Theguardians58 <125973002+Theguardians58@users.noreply.github.com>
2025-09-17 12:56:45 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+// This is the content for your homepage.
+// This will appear inside the master frame we created earlier.
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Éclat Lingerie</h1>
+      <p>This is the main page of our beautiful new website.</p>
+    </div>
+  );
+}
